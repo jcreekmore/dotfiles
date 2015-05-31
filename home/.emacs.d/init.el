1e91927be770de8f032db8563e3c52d29765803a
Jonathan Creekmore <jonathan@Jonathans-MacBook-Pro.local>
2015-05-30 19:43:48 -0500

    inhibit creation of backup files

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -56,3 +56,6 @@ Return a list of installed packages or nil for every skipped package."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;disable backup
+(setq backup-inhibited t)
